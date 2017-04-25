679d0d077dcf062e2784a7230f4e2e4951ea9193
saito20yukino <4yu8kk6iy9@gmail.com>
2017-04-25 18:49:44 +0900

    2nd commit.

---
+++ wwwroot/inquiry_fin.php
@@ -0,0 +1,53 @@
+<?php
+//inquiry_fin.php
+
+//入力された情報を取得
+/*$email = (string)@$_POST['email'];
+email = (string)filter_input(INPUT_POST, 'email';*/
+$params = array( 'email','name','birthday','body');
+$input_data = array();
+foreach($params  as  $p) {
+    $input_data[$p] = (string)@$_POST[$p];
+}
+var_dump($input_data);
+
+//validate(情報は正しい？）
+$error_detail = array();//エラー情報格納用変数
+
+//必須チェック
+$must_params = array('email', 'body');
+foreach($must_params  as  $p){
+     if('' === $input_data[$p]){
+	//エラー処理
+	$error_detail["error_must_{$p}"] = true;
+     }
+}
+
+//型チェック：email
+// XXXRFC日準拠のメアドはしらん！！
+if(false === filter_var($input_data['email'],FILTER_VALIDATE_EMAIL)) {
+    //エラー処理
+    $error_detail["error_format_email"] = true;
+}
+
+//型チェック：日付
+if('' !== $input_data['birthday']) {
+    if(false === strtotime($input_data['birthday'])) {
+	//エラー
+	$error_detail["error_format_birthday"] = true;
+    }
+}
+
+//エラー判定
+if(array() !== $error_detail){
+	//
+	echo 'エラーがあったらしい！！';
+	exit;
+}
+//ダミー
+echo 'でーたのvalidateはOKでした！！';
+//入力された情報をDBにinsert
+
+//「ありがとう」Pageの出力
+
+?>
